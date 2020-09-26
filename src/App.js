3aea665365b85a07d1fb568719bf46fbb51531d7
An Doan <dtan42@gmail.com>
2020-09-26 21:32:29 +1000

    Widget finish

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import SideBar from './SideBar';
+import Widgets from './Widgets'
 function App() {
+  const user = "An Doan";
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <SideBar />
-        <Feed />
-      </div>
+      {!user ? (
+        <h1>Login</h1>
+      ) :
+        <>
+          <Header />
+          <div className="app__body">
+            <SideBar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      }
+
     </div>
   );
 }
---
+++ src/Widgets.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Widgets.css';
+
+function Widgets() {
+    return (
+        <div className="widgets">
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpteintensive%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2257628174485398"
+                width="340"
+                height="500"
+                style={{ border: "none", overflow: "hidden" }}
+                scrolling="no"
+                frameborder="0"
+                allowTransparency="true"
+                allow="encrypted-media"></iframe>
+        </div>
+    )
+}
+
+export default Widgets
