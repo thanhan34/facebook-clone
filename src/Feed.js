72a9f7743c8b547948cd2ea720a05f8084b71ec7
An Doan <dtan42@gmail.com>
2020-09-26 17:59:18 +1000

    finish Message Sender

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
+            <MessageSender />
+
+            <Post />
         </div>
     )
 }
