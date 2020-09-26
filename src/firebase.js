3f91ee853e58761099d92ebf0c2d7eb5f0ac39f2
An Doan <dtan42@gmail.com>
2020-09-26 11:05:25 +1000

    set up base project

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKUWJRT-XRAC99h0hoPO8KGUJqgflZoFw",
+    authDomain: "facebook-clone-1bd4f.firebaseapp.com",
+    databaseURL: "https://facebook-clone-1bd4f.firebaseio.com",
+    projectId: "facebook-clone-1bd4f",
+    storageBucket: "facebook-clone-1bd4f.appspot.com",
+    messagingSenderId: "1057767275043",
+    appId: "1:1057767275043:web:f14af9e84283460fe4cf96",
+    measurementId: "G-GWG5TZD0Z9"
+  };
